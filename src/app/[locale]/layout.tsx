9876c7c5c3c4dbb1a52e61cecf385b2b0ccb6bf9
Daniil <dan.developer@born2be.media>
2025-09-29 16:07:05 +0300

    [feature/next-intl]: Next Intl (#1)

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,28 @@
+import { Inter } from 'next/font/google';
+import { NextIntlClientProvider } from 'next-intl';
+
+import { cn } from '@/shared/lib/utils/cn';
+
+import './globals.css';
+
+const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+});
+
+export default async function RootLayout({
+  children,
+  params,
+}: Readonly<{
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+}>) {
+  return (
+    <html lang={(await params).locale ?? 'en'}>
+      <body className={cn(inter.variable, 'antialiased')}>
+        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
