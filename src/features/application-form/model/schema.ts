b3ac44beb1e16bdf210ee9587f54c11c31f3996f
daniil-b2be <dan.developer@born2be.media>
2025-10-07 17:59:11 +0300

    add format block

--- src/features/application-form/model/schema.ts
+++ src/features/application-form/model/schema.ts
@@ -8,8 +8,23 @@ export const applicationFormSchema = v.object({
     v.email('Invalid email address'),
   ),
   linkedinProfile: v.pipe(v.string()),
-  coverLetter: v.optional(v.pipe(v.file())),
+  coverLetter: v.optional(
+    v.pipe(
+      v.file(),
+      v.mimeType(
+        [
+          'application/pdf',
+          'application/msword',
+          'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+        ],
+        'Cover letter must be PDF, DOC, or DOCX format',
+      ),
+    ),
+  ),
   position: v.pipe(v.string(), v.minLength(1, 'Position is required')),
-  resume: v.pipe(v.file('Resume is required')),
+  resume: v.pipe(
+    v.file('Resume is required'),
+    v.mimeType(['application/pdf'], 'Resume must be in PDF format'),
+  ),
   message: v.pipe(v.string()),
 });
